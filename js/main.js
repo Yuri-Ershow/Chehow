bb9e521b96c5a76d44c1d5862b2258641c111a80
eurij <er5hovyw@yandex.ru>
2021-07-17 13:07:14 +0300

    Added new files 17_07_21

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var x = document.getElementById("burger");
 x.addEventListener("click", myFunction);
 
 function myFunction() {
-    var element = document.getElementById("menu");
+    var element = document.getElementById("menu_main");
     element.classList.toggle("open");
     x.classList.toggle("burger_active");
 }
